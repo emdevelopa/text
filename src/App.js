32277dfb17e3642276950c78fd5590cc4b24154f
goodness5 <goodnesskolapo@gmail.com>
2023-05-04 15:46:14 +0100

    okay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react';
+
+import './index.css'
 
 function App() {
+  const [divs, setDivs] = useState([]);
+
+  const handleButtonClick = () => {
+    setDivs([...divs, <div key={divs.length} className='h-56 bg-black w-3/4'>a fucking div</div>]);
+  };
+
   return (
-    <div className="App">
-      <p onClick={add}>add</p>
-      <h1>default</h1>
+    <div className="App m-auto">
+      <button className='p-3 rounded-2xl bg-blue-200  justify-center' onClick={handleButtonClick}>Add a div</button>
+      <div className='grid grid-cols-4 gap-4'>{divs}</div>
     </div>
   );
 }
